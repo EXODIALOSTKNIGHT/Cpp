4d4feb95300ed5c9660cf35606a1438a8bce42ca
Nivelle Mendiola <Mendiolanivelle@gmail.com>
2018-10-09 18:21:28 +0800

    [CPP] DEV COURSE - 70 Moving objects with C++

---
+++ Source/Cpp/DoorOpen.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameFramework/Actor.h"
+#include "DoorOpen.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CPP_API UDoorOpen : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDoorOpen();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	void OpenDoorNow();
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+
+	UPROPERTY(EditAnywhere)
+		FRotator OpenAngle = FRotator(0, 0, 0);
+
+	AActor* Owner = GetOwner();
+	
+	
+	
+};
